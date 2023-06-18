9c6fabd042ae534b31af701bc89138d3d5830fbf
Sy2tem4 <bood00142@gmail.com>
2023-06-19 02:25:46 +0900

    Fix: github에서 vite기반으로 된 storybook이 정상적으로 빌드되지 않는 문제 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,9 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
