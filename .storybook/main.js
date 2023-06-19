ede4412fb779d0057d79fc17394d4a93b921e407
Sy2tem4 <bood00142@gmail.com>
2023-06-19 18:45:05 +0900

    Fix: 스토리북 빌드 오류 수정 시도
    - 오류 내용은 아래와 같습니다.
    
    error loading dynamically imported module: https://sy2tema.github.io/cdd-storybook-sy2tema/assets/config-4132e0f2.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/components/**/*.mdx",
+    "../src/components/**/*.stories.@(js|jsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
